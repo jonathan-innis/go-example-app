2a6096fbf0f2f0d22d5962838d598ea9455496d0
Jonathan Innis <jonathan.innis.ji@gmail.com>
2022-06-07 19:13:54 -0700

    Add initial app reading the secrets

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jonathan-innis/go-example-app
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	filePath = "/mnt/secret-store"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", ExampleHandler)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Printf("Failed to read the file from the path: %s", filePath)
+		w.Write([]byte("Error reading file"))
+		return
+	}
+	w.Write(append([]byte("Hello World! Here's my secret: "), data...))
+}
